7934cd1ca0b55d2fa6ff6beb3cb75f32343ead01
Alfred <avocadoaling@gmail.com>
2025-03-02 19:05:13 +0100

    feature: run predict on scaling dataset ablation

--- inference/engine.py
+++ inference/engine.py
@@ -44,7 +44,8 @@ def initialize_engine(
         lora_target_modules=lora_target_modules,
         tensor_parallel_size=tensor_parallel_size,
         load_format="bitsandbytes" if quantization else "auto",
-        max_model_len=8192,
+        # max_model_len=8192,
+        max_model_len=20000,
         gpu_memory_utilization=0.9
     )
 
