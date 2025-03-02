bec198eae05b8585f71c11ba46848fd63c4cd114
Alfred <avocadoaling@gmail.com>
2025-03-02 22:55:47 +0100

    add: script to copy submission folders

---
+++ run_scripts/run_copy_submission_epoch_ablation.sh
@@ -0,0 +1,5 @@
+
+# Batch=1
+cp -r /hkfs/work/workspace/scratch/tum_ind3695-arc-workspace/experiments_thesis_epoch_scaling/baseline_epoch_scaling_batch_1_output/ experiments_submission_files/
+# cp -r /hkfs/work/workspace/scratch/tum_ind3695-arc-workspace/experiments_thesis_epoch_scaling/baseline_epoch_scaling_batch_2_output/ experiments_submission_files/
+# cp -r /hkfs/work/workspace/scratch/tum_ind3695-arc-workspace/experiments_thesis_epoch_scaling/baseline_epoch_scaling_batch_3_output/ experiments_submission_files/
